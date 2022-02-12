4a78d8b62d6890a7072b5c8b2b36b40543fa4e03
delivey <deliveydev@gmail.com>
2022-02-12 20:40:28 +0200

    More minor code refactoring

--- main.cpp
+++ main.cpp
@@ -10,6 +10,52 @@ const std::unordered_map<int, char> players({
     {1, 'O'}
 });
 
+int enterMove(int player);
+void renderGameOver(int player);
+int checkForWin(std::unordered_map<int, char> &board)
+int makeMove(int move, int playerTurn, std::unordered_map<int, char> &board)
+
+int main() {
+    bool gameEnded = false;
+    bool playerTurn = 0;
+    std::unordered_map<int, char> board({
+        { 1, '1' }, { 2, '2' }, { 3, '3' },
+        { 4, '4' }, { 5, '5' }, { 6, '6' },
+        { 7, '7' }, { 8, '8' }, { 9, '9'}
+    });
+
+    while (!gameEnded) {
+        system("cls");
+        auto one = board.find(1)->second;
+        auto two = board.find(2)->second;
+        auto three = board.find(3)->second;
+        auto four = board.find(4)->second;
+        auto five = board.find(5)->second;
+        auto six = board.find(6)->second;
+        auto seven = board.find(7)->second;
+        auto eight = board.find(8)->second;
+        auto nine = board.find(9)->second;
+
+        const char *renderBoard =
+        "-------------------\n"
+        "|  %c  |  %c  |  %c  |\n"
+        "-------------------\n"
+        "|  %c  |  %c  |  %c  |\n"
+        "-------------------\n"
+        "|  %c  |  %c  |  %c  |\n"
+        "-------------------\n";
+        printf(renderBoard, one, two, three, four, five, six, seven, eight, nine);
+        int move = enterMove(playerTurn);
+        makeMove(move, playerTurn, board);
+        int gameOver = checkForWin(board);
+        if (gameOver) {
+            gameEnded = true;
+            renderGameOver(playerTurn);
+        }
+        playerTurn = !playerTurn;
+    };
+}
+
 int enterMove(int player) {
     int moved; 
     std::string playerS = std::to_string(player);
@@ -57,45 +103,4 @@ int makeMove(int move, int playerTurn, std::unordered_map<int, char> &board) {
     } else {
         return 1;
     }
-}
-
-int main() {
-    bool gameEnded = false;
-    bool playerTurn = 0;
-    std::unordered_map<int, char> board({
-        { 1, '1' }, { 2, '2' }, { 3, '3' },
-        { 4, '4' }, { 5, '5' }, { 6, '6' },
-        { 7, '7' }, { 8, '8' }, { 9, '9'}
-    });
-
-    while (!gameEnded) {
-        system("cls");
-        auto one = board.find(1)->second;
-        auto two = board.find(2)->second;
-        auto three = board.find(3)->second;
-        auto four = board.find(4)->second;
-        auto five = board.find(5)->second;
-        auto six = board.find(6)->second;
-        auto seven = board.find(7)->second;
-        auto eight = board.find(8)->second;
-        auto nine = board.find(9)->second;
-
-        const char *renderBoard =
-        "-------------------\n"
-        "|  %c  |  %c  |  %c  |\n"
-        "-------------------\n"
-        "|  %c  |  %c  |  %c  |\n"
-        "-------------------\n"
-        "|  %c  |  %c  |  %c  |\n"
-        "-------------------\n";
-        printf(renderBoard, one, two, three, four, five, six, seven, eight, nine);
-        int move = enterMove(playerTurn);
-        makeMove(move, playerTurn, board);
-        int gameOver = checkForWin(board);
-        if (gameOver) {
-            gameEnded = true;
-            renderGameOver(playerTurn);
-        }
-        playerTurn = !playerTurn;
-    };
 }
